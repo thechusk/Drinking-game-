5ef185097a1ec64ed15e5cb16ef3347bad179212
Tomas <tgirald7@github.com>
2018-03-11 17:28:43 -0500

    working MVC (still bugs)

--- Dicee/Dicee/Model/ThreemanDataModel.swift
+++ Dicee/Dicee/Model/ThreemanDataModel.swift
@@ -11,31 +11,89 @@ import UIKit
 class ThreemanDataModel {
 
     var message : String = ""
-    var randomDiceIndex1 : Int = 0
-    var randomDiceIndex2 : Int = 0
+    var index1 : Int = 0
+    var index2 : Int = 0
     let diceArray = ["dice1","dice2","dice3", "dice4","dice5","dice6"]
+    var sum : Int = 0
     
-    func combination (index1 : Int, index2: Int) -> String{
+//    func combination (index1 : Int, index2: Int) -> String{
+//
+//        if
+//            index1 == index2 {
+//
+//
+//        message = "When someone rolls double they give the dice to someone of their choosing. This person rolls the dice and must drink for that many seconds. The dice may be given to two people instead of giving both to just one person."
+//        }
+//
+//        else if index1+1 == 4 && index2+1 == 1 {
+//
+//       message = "Whoever rolls this become the Thumbmaster. They can place their thumb on the table whenever they want. The last person to do this must drink. The Thumbmaster can only change when someone else rolls a 4 & 1."}
+//
+//        else  if index1+1 == 1 && index2+1 == 4 {
+//            message = "Whoever rolls this become the Thumbmaster. They can place their thumb on the table whenever they want. The last person to do this must drink. The Thumbmaster can only change when someone else rolls a 4 & 1."}
+//
+//        return message
+//    }
+//
     
+    func updateMessage (sum : Int, index1: Int, index2: Int) ->  String {
+       
         if
             index1 == index2 {
             
-        
-        message = "When someone rolls double they give the dice to someone of their choosing. This person rolls the dice and must drink for that many seconds. The dice may be given to two people instead of giving both to just one person."
+            
+            message = "When someone rolls double they give the dice to someone of their choosing. This person rolls the dice and must drink for that many seconds. The dice may be given to two people instead of giving both to just one person."
+            return message
         }
-        
+            
         else if index1+1 == 4 && index2+1 == 1 {
-        
-       message = "Whoever rolls this become the Thumbmaster. They can place their thumb on the table whenever they want. The last person to do this must drink. The Thumbmaster can only change when someone else rolls a 4 & 1."}
+            
+            message = "Whoever rolls this become the Thumbmaster. They can place their thumb on the table whenever they want. The last person to do this must drink. The Thumbmaster can only change when someone else rolls a 4 & 1."
+            return message
+        }
             
         else  if index1+1 == 1 && index2+1 == 4 {
-            message = "Whoever rolls this become the Thumbmaster. They can place their thumb on the table whenever they want. The last person to do this must drink. The Thumbmaster can only change when someone else rolls a 4 & 1."}
+            message = "Whoever rolls this become the Thumbmaster. They can place their thumb on the table whenever they want. The last person to do this must drink. The Thumbmaster can only change when someone else rolls a 4 & 1."
+            return message
+        }
         
-        return message
+        switch sum {
+            
+        case  3 :
+            message = "Whoever is the three man must drink. If the three man rolls this then he may pass the title to a person of his choosing."
+           return message
+      
+        case 4 :
+      message = "Pass the dice!!"
+   return message
+        case  6 :
+ message = "Pass the dice!!"
+   return message
+        case 8 :
+ message = "Pass the dice!!"
+   return message
+        case 7 :
+ message = "Person to the right of “roller” drinks."
+   return message
+        case 9 :
+ message = "Person across from “roller” drinks."
+   return message
+        case 10 :
+ message = "Social…everyone drinks."
+           return message
+        case 11 :
+ message = "Person to the left of “roller” drinks."
+              return message
+
+        default :
+message = "please roll"
+            return message
+            
+        }
     }
-    
-    
-func updateDiceImage (sum : Int) ->  String {
+
+}
+
 //
 //        randomDiceIndex1 = Int(arc4random_uniform(UInt32(diceArray.count)))
 //        randomDiceIndex2 = Int(arc4random_uniform(UInt32(diceArray.count)))
@@ -65,35 +123,4 @@ func updateDiceImage (sum : Int) ->  String {
 //        else  if randomDiceIndex1+1 == 6 && randomDiceIndex2+1 == 6 {
 //            rulesLabel.text = "When someone rolls double they give the dice to someone of their choosing. This person rolls the dice and must drink for that many seconds. The dice may be given to two people instead of giving both to just one person."
 //        }
-        switch sum {
 
-        case  3 :
-           return "Whoever is the three man must drink. If the three man rolls this then he may pass the title to a person of his choosing."
-      
-        case 4 :
-        return "Pass the dice!!"
-
-        case  6 :
-return "Pass the dice!!"
-
-        case 8 :
-return "Pass the dice!!"
-
-        case 7 :
-return "Person to the right of “roller” drinks."
-
-        case 9 :
-return "Person across from “roller” drinks."
-
-        case 10 :
-return "Social…everyone drinks."
-            
-        case 11 :
-return "Person to the left of “roller” drinks."
-            
-        default:
-            return "roll dice"
-        }
-    }
-
-}
